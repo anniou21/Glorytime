6e3c3bfa396a0047d402b75fce3ae70bcd41654f
Etienne Testard <etitestard@gmail.Com>
2019-11-28 18:17:32 +0100

    save

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,6 +1,4 @@
 class Shop < ApplicationRecord
   belongs_to :user
   has_many :watches, dependent: :destroy
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
 end
