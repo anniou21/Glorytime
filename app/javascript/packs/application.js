3a0def83ce4b1c676f6171dafe526e25423e6d73
Etienne Testard <etitestard@gmail.Com>
2019-11-28 18:14:40 +0100

    Before_print_map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,44 +1,9 @@
 import "bootstrap";
 
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { initMapbox } from '../plugins/map';
+// import { initMapbox } from '../plugins/map';
 
 
-initMapbox();
+// initMapbox();
 
 
-const showOnlyPending = showPending => {
-  document.querySelectorAll('.pending').forEach( elt => {
-    if (showPending) {
-      elt.classList.remove('trip-hidden');
-    } else {
-      elt.classList.add('trip-hidden');
-    }
-  });
-  document.querySelectorAll('.confirmed').forEach( elt => {
-    if (showPending) {
-      elt.classList.add('trip-hidden');
-    } else {
-      elt.classList.remove('trip-hidden');
-    }
-  });
-};
-
-const attachTripButtonFilterBehavior = () => {
-  const button = document.getElementById("trip-status-filter-btn");
-
-  if (button) {
-    button.addEventListener("change", event => {
-      status = button.value;
-      if (status == 'pending') {
-        showOnlyPending(true);
-      } else {
-        showOnlyPending(false);
-      }
-    });
-  }
-};
-
-document.addEventListener('DOMContentLoaded', _ => {
-  attachTripButtonFilterBehavior();
-});
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,6 @@
 class Shop < ApplicationRecord
   belongs_to :user
   has_many :watches, dependent: :destroy
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
