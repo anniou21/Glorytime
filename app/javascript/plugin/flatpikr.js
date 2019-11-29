ca051f32adc7f4c8164e0271686eca75e3e40329
Cedrik Razafimanantsoa <cedrik.razafi@gmail.com>
2019-11-29 15:21:27 +0100

    Corrected datepicker

--- app/javascript/plugin/flatpikr.js
+++ app/javascript/plugin/flatpikr.js
@@ -1,6 +1,7 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css"; // Note this is important!
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
-flatpickr(".datepicker", {
-  mode: "range"
+flatpickr(".datepicker#booking_item_start_date", {
+  plugins: [new rangePlugin({ input: "#booking_item_end_date"})]
 })
