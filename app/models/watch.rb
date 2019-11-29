f848c837ab16d7e82fc24ba003afaa3b81585b08
ArafathNguyen <nguyen.arafath@gmail.com>
2019-11-29 15:10:34 +0100

    Searchbar implementation

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -3,4 +3,12 @@ class Watch < ApplicationRecord
   has_one_attached :photo
   has_many :booking_items
   has_many :bookings, through: :booking_items
+
+    include PgSearch::Model
+  pg_search_scope :search_by_model_and_brand,
+    against: [:model, :brand],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
+
