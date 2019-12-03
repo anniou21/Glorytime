26a3f7e5b20ac094bf8aa3c7014412a7e2995978
anniou toybou <anniou.toybou@gmail.com>
2019-12-03 16:31:38 +0100

    another fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_090658) do
+ActiveRecord::Schema.define(version: 2019_12_03_152633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,9 +52,9 @@ ActiveRecord::Schema.define(version: 2019_12_03_090658) do
   create_table "bookings", force: :cascade do |t|
     t.string "status"
     t.bigint "user_id"
-    t.integer "price_total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price_cents", default: 0, null: false
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
